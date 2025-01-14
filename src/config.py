d25cee56c8e99d033484910fa5d16ecc4232c899
honnigmelone <n.rosik@yahoo.de>
2025-01-14 05:25:26 +0100

    Added generator and save data to pickle and some basic setup

---
+++ src/config.py
@@ -0,0 +1,5 @@
+import os
+
+CURRENT_DIR = os.getcwd()
+PATH_TO_DIR = r"D:\data\image_data"
+OUTPUT_PATH = os.path.join(CURRENT_DIR, 'image_data.pkl')
---
+++ src/generator.py
@@ -0,0 +1,28 @@
+import os
+import pickle
+from tqdm import tqdm
+from config import PATH_TO_DIR, OUTPUT_PATH
+
+#Generator that yields a tuple of the root directory, filename, size
+def generator_to_pickle(path_to_dir):
+    for root, _, files in os.walk(path_to_dir):
+        for file in files:
+            if file.endswith((".png", ".jpg", ".jpeg")):
+                size = os.path.getsize(os.path.join(root, file))
+                yield root, file, size
+
+
+def save_data_to_pickle(gen, output_path):
+    image_id = 0
+    image_metadata_list = []
+    for root, file, size in tqdm(gen, desc="Processing"):
+        image_metadata = {"root": root, "file": file, "size": size}
+        image_metadata_list.append(image_metadata)
+        image_id += 1
+
+    with open(output_path, "wb") as f:
+        pickle.dump(image_metadata_list, f)
+
+
+gen = generator_to_pickle(PATH_TO_DIR)
+save_data_to_pickle(gen, OUTPUT_PATH)
