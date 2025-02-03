ba14660d546a598ebd6e9ed5af23e79a9c098a52
honnigmelone <n.rosik@yahoo.de>
2025-02-03 17:35:32 +0100

    small additions

--- src/config.py
+++ src/config.py
@@ -6,3 +6,4 @@ PATH_TO_IMAGE_DATA = r"D:\data\image_data"
 DATA_ROOT = os.path.join(PROJECT_ROOT, "data")
 IMAGE_DATA_OUTPUT_PATH = os.path.join(DATA_ROOT, "image_data.pkl")
 DATABASE_PATH = os.path.join(DATA_ROOT, "image_database.db")
+LOGS_ROOT = os.path.join(PROJECT_ROOT, "logs")
