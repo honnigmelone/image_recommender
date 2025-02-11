d4b34f75f09c2f238ee32a8e662af11cff04e0c7
heyitsalina <alina.weidemann04@gmail.com>
2025-02-11 21:36:50 +0100

    some changes lol

--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -4,6 +4,7 @@ from unittest.mock import patch, MagicMock
 from PIL import Image
 import torch
 import sys
+import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 from embeddings import process_image, preprocess, get_embedding, input_image_embedding
